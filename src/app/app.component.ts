477e6fe27468e2291488d2ab35a1e3ab8acaf63e
Peterkal01 <48078237+Peterkal01@users.noreply.github.com>
2020-11-29 21:33:42 +1100

    almost done lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+
+function snapshotToData(snapshotObservable: Observable<any>) {
+  return snapshotObservable.pipe(map(actions => actions.map((a) => ({
+    id: a.payload.doc.id,
+    data: a.payload.doc.data(),
+  }))));
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'BasketballAppDemo';
+
+  email: string = "";
+  password: string = "";
+  venue: string = "";
+  payment: number = 0;
+  payer: string = "";
+  games;
+
+  constructor(
+    private auth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) {
+    this.games = snapshotToData(firestore.collection("games").snapshotChanges());
+  }
+  
+  async registerWithEmailAndPassword(){
+    let register = await this.auth.createUserWithEmailAndPassword(this.email, this.password);
+    await this.createUser(register.user.uid, register.user.email);
+  }
+
+  async loginWithEmailAndPassword(){
+     console.log(await this.auth.signInWithEmailAndPassword(this.email, this.password));
+  }
+
+  async loginWithGoogle() {
+    let login = await this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    await this.createUser(login.user.uid, login.user.email);
+    console.log(login);
+  }
+
+  async createUser(uid, email) {
+    if (!(await this.firestore.collection('users').doc(uid).get().toPromise()).data())
+    {
+      await this.firestore.collection('users').doc(uid).set({
+        email: email,
+        total: 0,
+        state: 'pending',
+      });
+    }
+  }
+
+  async logout() {
+    this.auth.signOut();
+  }
+
+  createGame(){
+    this.firestore.collection('games').add({
+      venue: this.venue,
+      payment: 0, 
+    })
+  }
+
+  deleteGame(id){
+    this.firestore.collection("games").doc(id).delete();
+  }
+
+  confirmPayment(id){
+    this.firestore.collection('games').doc(id).update({
+      payment: this.payment,
+      payer: this.payer,
+    })  
+  }
 }
---
+++ src/environments/environment.common.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyD4IZyM-NKJyU0tDXqyf4PkFMJ7fll-CGs',
+    authDomain: 'basketballapp-77ffe.firebaseapp.com',
+    databaseURL: 'https://basketballapp-77ffe.firebaseio.com',
+    projectId: 'basketballapp-77ffe',
+    storageBucket: 'basketballapp-77ffe.appspot.com',
+    messagingSenderId: '968484496039',
+    appId: '1:968484496039:web:a845642eccb14515777182'
+  },
+};
